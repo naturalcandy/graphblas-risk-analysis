40c371c7e0caa33bb0369aa2ac3a7eb97f482c9d
Nolawi Gerawork <nolawigerawork@gmail.com>
2023-08-11 22:19:45 -0400

    adds logic to generate distress for simulation

--- data_utils/data_formatting.py
+++ data_utils/data_formatting.py
@@ -1,52 +1,79 @@
 import csv
 import graphblas as gb
 gb.init("suitesparse", blocking=True)
-from graphblas import Matrix, Vector
+from graphblas import Matrix, Vector, binary
 
-def read_financial_data(filename="data/raw/synthetic_data.csv"):
+FILE_EDGES = "data/raw/synthetic_data.csv"
+FILE_NODES = "data/raw/synthetic_nodes.csv"
+
+
+def read_financial_data(file_edges : str, file_nodes : str) -> (Matrix, Vector):
     """
-    Reads financial data from a CSV file and returns a GraphBLAS matrix and vector.
-    
-    The CSV file is expected to have the following columns:
-    - Node1: The ID of the first node (institution) in an edge.
-    - Node2: The ID of the second node (institution) in an edge.
-    - Exposure: The exposure value between Node1 and Node2.
-    - Capital: The capital value of Node1.
-    
-    The function returns:
-    - A GraphBLAS matrix L where each entry L_{ij} represents the exposure from institution i to institution j.
-    - A GraphBLAS vector C where each entry C_i represents the capital of institution i.
+    Reads financial data from provided CSV files and returns a GraphBLAS matrix and vector.
     
     Parameters:
-    - filename (str): The path to the CSV file containing the financial data. 
-                      Default is "data/raw/synthetic_data.csv".
+    - file_edges (str): The path to the CSV file containing the financial data. 
+                      
+    - file_nodes (str): The path to the CSV file containing the financial nodes.
     
     Returns:
-    - L (grblas.Matrix): A GraphBLAS matrix representing exposures between institutions.
-    - C (grblas.Vector): A GraphBLAS vector representing the capital of each institution.
+    - L (gb.Matrix): A GraphBLAS matrix representing exposures between institutions.
+    - c (gb.Vector): A GraphBLAS vector representing the capital of each institution.
     """
+    #create our asset liability matrix
     edges = []    
-    with open(filename, 'r') as csvfile:
+    with open(file_edges, 'r') as csvfile:
         reader = csv.reader(csvfile)
-        next(reader)  # Skip the header
+        next(reader)
         for row in reader:
-            node1, node2, exposure, capital = int(row[0]), int(row[1]), float(row[2]), float(row[3])
-            # we assume that the capital of a node k is located in the kth row of the data
-            edges.append((node1, node2, exposure, capital))
+            node1, node2, exposure = int(row[0]), int(row[1]), float(row[2])
+            edges.append((node1, node2, exposure))
     
     row_indices = [edge[0] for edge in edges]
     col_indices = [edge[1] for edge in edges]
     values = [edge[2] for edge in edges]
-    capital_values = [edge[3] for edge in edges]
     
     L = Matrix.from_coo(row_indices, col_indices, values, dtype=float)
+
+    #create our capital vector
+    nodes = []
+    with open(file_nodes, 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        next(reader)
+        for row in reader:
+            node1, capital = int(row[0]), float(row[1])
+            nodes.append((node1, capital))
+        node_indices = [node[0] for node in nodes]
+        capital_indices = [node[1] for node in nodes]
+
+        c = Vector.from_coo(node_indices, capital_indices)
     
-    capital_indices = list(range(len(capital_values)))
-    C = Vector.from_coo(capital_indices, capital_values)
-    
-    return L, C
+    return L, c
 
 
-L, C = read_financial_data()
-print(L)
-print(C)
+def generate_distress(num_institutions: int, distress_vector: Vector):
+    """
+    Simulates the initial financial distress.
+
+    Parameters:
+    - num_institutions (int): The number of institutions.
+    - distress_vector (gb.Vector): A vector of floats representing the initial shock, 's'
+                                   applied to each institution. 
+
+    Returns:
+    - h (gb.Vector): A vector represenitng the health of all insitiutions after the initial shock
+    - s (gb.Vector): A vector representing the state of all institutions after the initial shock
+    """
+    #state vector is discrete whereas health can range from 0 - 1
+    health_vector = Vector(float, num_institutions)
+    state_vector = Vector(int, num_institutions)
+    health_vector[:] << 0
+    state_vector[:] << 0
+
+    #apply initial distress
+    health_vector << health_vector.ewise_mult(distress_vector, binary.plus)
+    distressed_institutions = health_vector.apply(lambda x: 1 if x != 0 else 0).new()
+    state_vector << state_vector.ewise_mult(distressed_institutions, binary.bor)   
+
+    return health_vector, state_vector
+
